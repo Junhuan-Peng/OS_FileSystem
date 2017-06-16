1c317de44c25879221b002cce18f179ddacf5a11
Junhuan-Peng <13061638633@163.com>
2017-06-16 22:52:31 +0800

    添加项目文件。

---
+++ OS课程设计_彭浚桓_1151250213/MyFileSystem.cpp
@@ -0,0 +1,125 @@
+// MyFileSystem.cpp : 定义控制台应用程序的入口点。
+//
+
+
+#include "MyFileSystem.h"//定义基本结构
+#include <iostream>
+
+void usage();
+Cmd* Cmd::instance = nullptr;
+
+int main(int args, char* argv[]){
+	cout << "输入 help 获取帮助" << ",输入 Exit 退出" << endl;
+	string input_cmd;
+	Cmd* cmd = Cmd::getInstance();
+	while (true){
+		cout << ">>";
+		getline(cin, input_cmd);
+		if (input_cmd._Equal("Exit") || input_cmd._Equal("exit"))
+			break;
+		if (input_cmd._Equal("help")){
+			usage();
+			continue;
+		}
+
+		auto flag = cmd->parse(input_cmd);
+		if (!flag){
+			cout << "Error" << endl;
+		}
+	}
+	return 0;
+}
+
+bool Cmd::parse(string cmd){
+	string* strings = split(cmd, ' ');
+	if (strings[0]._Equal("Format")){
+		Format();
+	} else if (strings[0]._Equal("MKfile")){
+		MkFile(strings[1]);
+	} else if (strings[0]._Equal("MKdir")){
+		MkDir(strings[1]);
+	} else if (strings[0]._Equal("Cd")){
+		Cd(strings[1]);
+	} else if (strings[0]._Equal("Delfile")){
+		DelFile(strings[1]);
+	} else if (strings[0]._Equal("Dir")){
+		Dir();
+	} else if (strings[0]._Equal("Copy")){
+		Copy(strings[1],strings[2]);
+	} else if (strings[0]._Equal("Open")){
+		Open(strings[1]);
+	} else if (strings[0]._Equal("Attrib")){
+		Attrib(strings[1],strings[2]);
+	} else if (strings[0]._Equal("Viewinodemap")){
+		ViewINodeMap();
+	} else if (strings[0]._Equal("Viewblockmap")){
+		ViewBlockMap();
+	} else{
+		cout << strings[0] << " 找不到命令！请重新输入" << endl;
+	}
+	return true;
+}
+
+bool Cmd::Format(){
+	return true;
+}
+
+bool Cmd::MkFile(string cmd){
+	return true;
+}
+
+bool Cmd::MkDir(string cmd){
+	return true;
+}
+
+bool Cmd::Cd(string cmd){
+	return true;
+}
+
+bool Cmd::DelFile(string cmd){
+	return true;
+}
+
+bool Cmd::DelDir(string cmd){
+	return true;
+}
+
+bool Cmd::Dir(){
+	return true;
+}
+
+bool Cmd::Copy(string orign_path, string goal_path){
+	return true;
+}
+
+bool Cmd::Open(string cmd){
+	return true;
+}
+
+bool Cmd::Attrib(string file_path, string operation){
+	return true;
+}
+
+bool Cmd::ViewINodeMap(){
+	return true;
+}
+
+bool Cmd::ViewBlockMap(){
+	return true;
+}
+
+
+void usage(){
+	cout << "Format\t\t\t初始化磁盘，划定结构" << endl <<
+		"Mkfile\t\t\t创建文件" << endl <<
+		"Mkdir\t\t\t创建目录" << endl <<
+		"Cd\t\t\t改变当前目录" << endl <<
+		"Delfile [file path]\t删除文件（注意只读属性）" << endl <<
+		"Deldir [dir path]\t删除目录（注意只读属性）" << endl <<
+		"Dir\t\t\t列文件目录 （列出名字和建立时间，注意隐藏属性）" << endl <<
+		"Copy [origin file path] [goal file path]\t\t\t复制文件到某一路经" << endl <<
+		"Open [file path]\t打开并编辑文件（注意只读属性）" << endl <<
+		"Attrib [+r|-r] [+h|-h]  [file path]\t\t更改文件属性，加只读，减只读，加隐藏，减隐藏" << endl <<
+		"Viewinodemap\t\t显示当前inode位示图状况" << endl <<
+		"Viewblockmap\t\t显示当前block位示图状况" << endl;
+}
---
+++ OS课程设计_彭浚桓_1151250213/MyFileSystem.h
@@ -0,0 +1,158 @@
+#pragma once
+
+#include <string>
+
+struct TxtBlock;
+using namespace std;
+
+// TODO: 在此处引用程序需要的其他头文件
+
+//一些常量的定义
+const int BLOCK_SIZE = 64; //磁盘块的大小——64字节
+const int DIRECORY_ENTRY = 16;//目录项的大小——16字节
+const int I_NODE_BITMAP_SIZE = BLOCK_SIZE;//i-node 位图 一块64字节（共512位）,含有一个磁盘块
+
+struct Disc;
+struct RootDirectory;
+struct I_NodeBitmap;
+struct BlockBitmap;
+struct I_NODE;
+union DataBlock;
+
+struct DirecoryEntry;
+struct IndexBlock;
+struct TxtBlock;
+struct DirectoryFileBlock;
+
+//目录项
+struct DirecoryEntry
+{
+	char fileName[16];//文件或目录名
+	unsigned char flag;//0 表示一般文件，1 表示目录
+	int i_node_number;//i-node 编号——指向文件或者目录的索引节点
+};
+
+//索引数据块
+struct IndexBlock
+{
+	int indexs[16];
+};
+//文本文件数据块
+struct TxtBlock
+{
+	char txt[64];
+};
+//目录文件数据块
+struct DirectoryFileBlock
+{
+	DirecoryEntry direcoryEntry[4];
+};
+
+union DataBlock
+{
+	IndexBlock indexBlock;//索引块
+	TxtBlock txtBlock;//文本文件数据块
+	DirectoryFileBlock directoryBlock;//目录文件数据块
+};
+
+
+//根目录——最多四个目录项
+struct RootDirectory
+{
+	DirecoryEntry direcoryEntry[4];
+};
+
+//i-node位图
+struct I_NodeBitmap
+{
+	bool i_node[I_NODE_BITMAP_SIZE * 8];
+};
+
+//数据块位图——描述磁盘的状态
+struct BlockBitmap
+{
+	bool BlocksStates[512 * BLOCK_SIZE];
+};
+
+//i-node 长度为16字节（1+1+1+1+2*4+4）
+//在文件超出两个盘块时，将使用一级索引地址，就是说文件最大为18个盘块
+struct I_NODE
+{
+	unsigned char isReadOnly;// 是否只读
+	unsigned char isHide;// 是否隐藏
+	unsigned char hour;// 建立文件系统时间的小时
+	unsigned char minutes;// 建立文件系统时间的分钟
+	int directAddress[2];// 直接盘块地址——指向一个数据块
+	int firstClassIndexAddress;// 一级索引地址——指向一个索引块
+};
+
+//磁盘
+struct Disc
+{
+	RootDirectory rootDirectory;
+	I_NodeBitmap i_nodeBitMap;
+	BlockBitmap blockBitMap[2];//描述1024块磁盘块的状态
+	I_NODE i_node[512];//512个i-node占用磁盘128块
+	DataBlock dataBlock[1024];
+};
+
+
+
+
+class Cmd{
+public:
+	
+	~Cmd();
+	bool parse(string cmd);
+	static Cmd* getInstance();
+	string* split(string s, char c);
+private:
+	Cmd();
+	static Cmd *instance;
+	bool Format();//初始化磁盘，划定结构
+	bool MkFile(string cmd);//创建文件
+	bool MkDir(string cmd);//创建目录
+	bool Cd(string cmd);//改变当前目录
+	bool DelFile(string cmd);//删除文件（注意只读属性）
+	bool DelDir(string cmd);//删除目录
+	bool Dir();//列文件目录
+	bool Copy(string orign_path,string goal_path);//复制文件到某一路径
+	bool Open(string cmd);//打开并编辑文件
+	bool Attrib(string file_path,string operation);//更改文件属性（是否为只读、是否被隐藏）
+	bool ViewINodeMap();
+	bool ViewBlockMap();
+
+};
+
+inline Cmd::Cmd(){
+}
+
+inline Cmd::~Cmd(){
+}
+
+inline Cmd* Cmd::getInstance(){
+	if (instance == nullptr){
+		instance = new Cmd();
+	}
+
+	return instance;
+}
+
+inline string* Cmd::split(string s, char c){
+	string* strings = new string[3];
+	string temps(s);
+	for (int i = 0,j=0;;j++)
+	{
+		if ((i = temps.find_first_of(c))!=string::npos){
+			strings[j] = string(temps, 0, i);
+			temps = string(temps, i + 1);
+		}
+		else
+		{
+			strings[j] = string(temps, 0, i);
+			break;
+		}
+	}
+	return strings;
+}
+
